bdfb0c27cf477a72f8797823f107584010357c7f
sandhya v <vsandhya5950@gmail.com>
2021-11-25 11:01:03 +0530

    mongo cookies added

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require( 'express' );
+const app = express();
+const mongoose = require( 'mongoose' );
+const exphbs = require( 'express-handlebars' );
+// const session = require('express-session')
+const cookieparser = require('cookie-parser')//it takes data from cookies,if its not there it shows undefined
+const port = 7000;
+const uname = 'sandhya'
+const pswd = 'sandhya'
+//session middleware
+const oneDay = 24*60*60*1000
+/* app.use(session({
+    secret:'thisisasecretkey',
+    saveUninitialized:true,
+    resave:false,
+    cookie:{
+        maxAge:oneDay
+    }
+})) */
+
+
+
+
+
+//cookie-parser
+app.use(cookieparser())
+
+
+
+
+
+app.use(express.urlencoded({extended:true}))
+
+
+// const dbUrl = 'mongodb+srv://admin:admin@cluster0.mmcdv.mongodb.net/products-App?retryWrites=true&w=majority';
+
+const dbUrl = 'mongodb://localhost:27017/products-App';
+
+mongoose.connect( dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+    ( err ) => {
+        if ( !err ) {
+            console.log( 'DB connection was successful ' );
+        } else {
+            console.log( 'DB connection failed' );
+        }
+    } );
+
+// importing products route
+const productsRoute = require('./routes/products')
+// const orderRoute = require('./routes/orders')
+
+// set up template engine
+app.engine( 'handlebars', exphbs() );
+app.set( 'view engine', 'handlebars' );
+
+
+
+// body parser middleware
+app.use( express.urlencoded( { extended: true } ) );
+
+// router level middleware
+app.use('/products',productsRoute)
+
+app.get( '/error', ( req, res ) => {
+    res.status(500).send('Something went wrong')
+})
+
+app.get('/',(req,res)=>{
+    /* if(req.session.userid){
+        res.redirect('/')
+    }else{
+        res.render('./login.handlebars')
+    } */
+    res.render('./login.handlebars')
+
+})
+app.get("/login",(req,res)=>{
+
+    if(req.cookies.myname===uname && req.cookies.password===pswd){
+        res.render('./landingpage.handlebars')
+    }else{
+        res.redirect('/')
+    }
+   
+})
+
+app.post('/login',(req,res)=>{
+    const {username,password,checkbox} =req.body
+
+    //logic  to create cookies or not 
+  
+    if(uname === username && pswd === password){
+        console.log(req.session);
+        res.render('./landingpage.handlebars');
+        if(checkbox){
+
+            res.cookie("myname",username,{
+                maxAge:36000
+            })
+            res.cookie("password",password,{
+                maxAge:36000
+            })
+        }
+
+    }else{
+        res.redirect('/')
+    }
+
+ 
+
+})
+app.get("/logout", (req,res)=>{
+    // req.session.destroy();
+    res.redirect("/")
+});
+
+
+app.listen( port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
